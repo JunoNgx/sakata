c18c77e2a031a6789099d65381fdd1dcf903cab9
JunoNgx <juno.ngx@gmail.com>
2021-01-16 14:09:10 +0800

    Enable audio with pin C6

--- config.h
+++ config.h
@@ -1,20 +1,3 @@
-/*
-Copyright 2019 Juno Nguyen
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 #include "config_common.h"
@@ -43,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #define MATRIX_ROW_PINS {D5, F6, F7, B0}
 #define MATRIX_COL_PINS {F0, E6, B3, B7, D1, D0, D6, D4, D3, D2, D7, B4}
-#define UNUSED_PINS {B5, B6, C6, C7, B1, B2}
+#define UNUSED_PINS {B5, B6, C7, B1, B2}
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
@@ -249,3 +232,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+
+#define C6_AUDIO
