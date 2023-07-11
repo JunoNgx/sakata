c770c3274151d06575b682d465bf9b04cc4ec91b
Juno Nguyen <juno.ngx@gmail.com>
2023-07-11 22:45:00 +0800

    Clean up comments

--- r/config.h
+++ r/config.h
@@ -1,49 +1,4 @@
-// #pragma once
-
-// #include "config_common.h"
-
-// #define MATRIX_ROWS 4
-// #define MATRIX_COLS 12
-// #define MATRIX_ROW_PINS {D5, F5, F6, F7}
-// #define MATRIX_COL_PINS {F0, E6, B3, B7, D0, D1, D2, D3, D4, D6, D7, B4}
 #define AUDIO_PIN C6
-
-// #define DIODE_DIRECTION COL2ROW
-
-// // #define BACKLIGHT_PIN B7
-// // #define BACKLIGHT_BREATHING
-// // #define BACKLIGHT_LEVELS 3
-
-// #define SOFT_SERIAL_PIN D0 // or D1, D2, D3, E6
-
-// #define RGB_DI_PIN C7
-// #ifdef RGB_DI_PIN
-//   #define RGBLED_NUM 16
-//   #define RGBLIGHT_HUE_STEP 8
-//   #define RGBLIGHT_SAT_STEP 8
-//   #define RGBLIGHT_VAL_STEP 8
-//   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
-//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-
-//   #define RGBLIGHT_EFFECT_BREATHING
-//   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-//   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-//   #define RGBLIGHT_EFFECT_SNAKE
-//   #define RGBLIGHT_EFFECT_KNIGHT
-//   #define RGBLIGHT_EFFECT_CHRISTMAS
-//   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//   #define RGBLIGHT_EFFECT_RGB_TEST
-//   #define RGBLIGHT_EFFECT_ALTERNATING
-// // /*== customize breathing effect ==*/
-// //   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
-// //   #define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
-// //   /*==== use exp() and sin() ====*/
-// //   #define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
-// //   #define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
-// #endif
-
-// /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-// #define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
